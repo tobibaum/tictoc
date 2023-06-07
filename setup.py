250ce2afe1243bfa297dccf814d895f3f49cc72d
Tobias Baumgartner <tobibaum@gmail.com>
2023-06-07 09:49:58 +0200

    install requirements

--- setup.py
+++ setup.py
@@ -8,8 +8,16 @@ base_version_filename = 'VERSION'
 with open(base_version_filename) as fh:
     VERSION = fh.read().strip()
 
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = lib_folder + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
+
 setup(name='tictoc',
       version=VERSION,
+      install_requires=install_requires,
       url='https://github.com/tobibaum/tictoc',
       description='''tictoc tool for nondeterministic program paths and memory
                      consumption''',
